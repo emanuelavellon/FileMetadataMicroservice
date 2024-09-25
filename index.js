923b40834da53061d796bc40cc068978165d0570
Emanuel Avellón López <emanuel.avellon@gmail.com>
2024-09-25 10:59:21 +0000

    [OPT] - Code Identation

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer=require('multer');
+
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const upload=multer({
+  dest: 'uploads/'
+});
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  
+if(!req.file) return;
+
+  const messageRespose={
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+
+  res.json(messageRespose);
+});
+
+
 
 
 
